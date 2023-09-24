b739422e9f376c9fa9d86fbddaeac9c6190445d8
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-24 23:30:19 +0530

    Update create-hello-world-function.js

--- create-hello-world-function.js
+++ create-hello-world-function.js
@@ -19,19 +19,7 @@ Constraints:
 
 //solution"
 
-var createCounter = function(n) {
-    return function() {
-        return n++;
-    };
-};
 
-const counter = createCounter(10)
-counter()
-counter()
-counter()
-/** 
- * const counter = createCounter(10)
- * counter() // 10
- * counter() // 11
- * counter() // 12
- */
+function createHelloWorld() {
+  return function() { return "Hello World" }
+}
