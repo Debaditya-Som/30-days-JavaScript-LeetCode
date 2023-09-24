c7fd024e1f8ad2939aa646c4c5f06f9f11607ac9
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-25 00:09:42 +0530

    Update apply-transform-over-each-element-in-array.js

--- apply-transform-over-each-element-in-array.js
+++ apply-transform-over-each-element-in-array.js
@@ -1,5 +1,7 @@
 /*
 
+2635. Apply Transform Over Each Element in Array
+
 Given an integer array arr and a mapping function fn, return a new array with a transformation applied to each element.
 
 The returned array should be created such that returnedArray[i] = fn(arr[i], i).
