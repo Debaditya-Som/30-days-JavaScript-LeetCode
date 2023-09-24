f94d379959057bf6730ac4e9f96b2aed945e5938
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-25 00:07:56 +0530

    Update counter.js

--- counter.js
+++ counter.js
@@ -1,4 +1,6 @@
 /*
+2620. Counter
+
 Given an integer n, return a counter function. This counter function initially returns n and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
 Example 1:
 
