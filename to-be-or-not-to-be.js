d43e9f628dff5c2532c278759b1076da44c78d2b
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-25 00:09:08 +0530

    Update to-be-or-not-to-be.js

--- to-be-or-not-to-be.js
+++ to-be-or-not-to-be.js
@@ -1,4 +1,7 @@
 /*
+
+2704. To Be Or Not To Be
+
 Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
 The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
 The function composition of an empty list of functions is the identity function f(x) = x.
