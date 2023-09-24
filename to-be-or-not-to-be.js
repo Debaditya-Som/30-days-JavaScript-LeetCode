72f63355b55ccb868ede890c43331a453038e910
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-24 23:33:55 +0530

    Added to-be-or-not-to-be.js
    Solution to problem no. 2704
    
    Write a function expect that helps developers test their code. It should take in any value val and return an object with the following two functions.
    
    toBe(val) accepts another value and returns true if the two values === each other. If they are not equal, it should throw an error "Not Equal".
    notToBe(val) accepts another value and returns true if the two values !== each other. If they are equal, it should throw an error "Equal".

---
+++ to-be-or-not-to-be.js
@@ -0,0 +1,39 @@
+/*
+Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+The function composition of an empty list of functions is the identity function f(x) = x.
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+Example 1:
+
+Input: functions = [x => x + 1, x => x * x, x => 2 * x], x = 4
+Output: 65
+Explanation:
+Evaluating from right to left ...
+Starting with x = 4.
+2 * (4) = 8
+(8) * (8) = 64
+(64) + 1 = 65
+
+Constraints:
+
+-1000 <= x <= 1000
+0 <= functions.length <= 1000
+all functions accept and return a single integer
+
+*/
+
+//solution
+
+var expect = function(val) {
+    return {
+        toBe: (val2) => {
+            if (val !== val2) throw new Error("Not Equal");
+            else return true;
+        },
+        notToBe: (val2) => {
+            if (val === val2) throw new Error("Equal");
+            else return true;
+        }
+    }
+};
