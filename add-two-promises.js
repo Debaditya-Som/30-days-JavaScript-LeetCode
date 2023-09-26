3c1b696adf5acedefaf87291d5b737f9bba5c2a4
Debaditya Som <121785700+Debaditya-Som@users.noreply.github.com>
2023-09-26 17:50:11 +0530

    Added add-two-promises.js
    Added 2723

---
+++ add-two-promises.js
@@ -0,0 +1,19 @@
+/*
+2723. Add Two Promises
+
+Given two promises promise1 and promise2, return a new promise. promise1 and promise2 will both resolve with a number. The returned promise should resolve with the sum of the two numbers.
+ 
+
+Example 1:
+
+Input: 
+promise1 = new Promise(resolve => setTimeout(() => resolve(2), 20)), 
+promise2 = new Promise(resolve => setTimeout(() => resolve(5), 60))
+Output: 7
+Explanation: The two input promises resolve with the values of 2 and 5 respectively. The returned promise should resolve with a value of 2 + 5 = 7. The time the returned promise resolves is not judged for this problem.
+
+*/
+var addTwoPromises = async function(promise1, promise2) {
+    const [value1, value2] = await Promise.all([promise1, promise2]);
+    return value1 + value2;
+};
